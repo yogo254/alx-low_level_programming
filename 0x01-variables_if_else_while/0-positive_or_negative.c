38ddfd3482fef9ce79f95e1935d4c534fee790e0
yogo254 <yogo254@yahoo.com>
2022-03-11 22:02:14 +0300

    fix indentations

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main()
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    /* your code goes there */
-
-    if (n > 0)
-        printf("is positive");
-    else if (n == 0)
-        printf("is zero");
-    else
-        printf("is negative");
-
-    printf("\n");
-
-    return 0;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive", n);
+	else if (n == 0)
+		printf("%d is zero", n);
+	else
+		printf("%d is negative", n);
+	printf("\n");
+	return (0);
 }
