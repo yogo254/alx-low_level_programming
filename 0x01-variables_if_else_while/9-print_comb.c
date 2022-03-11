8c07b2bbb334207bc3209645a2ff654753ea97c6
yogo254 <yogo254@yahoo.com>
2022-03-12 00:43:47 +0300

    add list delimetered digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - main function
+ * @void: no params
+ *
+ * Description: main function.
+ * Return: exist status
+ */
+int main(void)
+{
+	int letter;
+
+	letter = 0;
+	for (; letter < 10; letter++)
+	{
+		putchar((letter % 10) + '0');
+		if (letter < 9)
+			putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
