4e882028a7040b78927e762ae2ff9d1fc1a43b81
yogo254 <yogo254@yahoo.com>
2022-03-11 23:53:06 +0300

    add alphabets filter

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - main function
+ * @void: no params
+ *
+ * Description: main function.
+ * Return: exist status
+ */
+int main(void)
+{
+	int letter;
+
+	letter = 'a';
+	for (; letter < (97 + 26); letter++)
+	{
+		if (letter != 101 || letter != 113)
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
