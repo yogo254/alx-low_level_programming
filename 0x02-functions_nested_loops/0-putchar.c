7bbc73819d50c2577b0313c5ca74f75cd4c2ea97
yogo254 <yogo254@yahoo.com>
2022-03-15 19:53:19 +0300

    add put char output

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - main function
+ * @void: no params
+ *
+ * Description: main function.
+ * Return: exist status
+ */
+int main(void)
+{
+	prinf("_putchar");
+	return (0);
+}
+
