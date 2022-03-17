c9ca922115f1e97329b6e3c14b88d490805935c2
yogo254 <yogo254@yahoo.com>
2022-03-17 10:58:15 +0300

    add print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+/**
+ * print_diagonal - print diagonal.
+ * @n: diagonal size
+ * Return: returns nothing.
+ */
+void print_diagonal(int n)
+{
+
+	int count = 0;
+
+	if (n > 0)
+		while (count < n)
+		{
+			_putchar('\\');
+		}
+	_putchar('\n');
+}
