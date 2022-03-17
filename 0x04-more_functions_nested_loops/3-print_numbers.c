b0739043be17a82480b6460dd54b610d9c9e51aa
yogo254 <yogo254@yahoo.com>
2022-03-17 10:10:40 +0300

    add  print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+/**
+ * print_numbers - print digits.
+ * @void: empty param
+ * Return: returns nothing.
+ */
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		_putchar(num);
+		_putchar('\n');
+	}
+}
