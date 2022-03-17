26d164a40fb73c8c902f20f4e625f7452ea66135
yogo254 <yogo254@yahoo.com>
2022-03-17 09:58:34 +0300

    add get number product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/**
+ * _mul - find product of two numbers.
+ * @a: first number
+ * @b: second number
+ * Return: product of a and b.
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
