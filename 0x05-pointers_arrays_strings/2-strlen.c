773a89ee60d76e001972379bda6a85286e177f09
yogo254 <yogo254@yahoo.com>
2022-03-21 20:27:32 +0300

    add get string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <string.h>
+/**
+ * _strlen - get string length
+ * @s: String pointer
+ *
+ * Description: get string lenght of a pointer.
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	char text = s;
+
+	int len = strlen(text);
+
+	return (len);
+}
