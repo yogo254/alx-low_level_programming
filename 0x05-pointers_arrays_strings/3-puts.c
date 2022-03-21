90c927beafd5b4051bd431cae6eb22e50258ee3e
yogo254 <yogo254@yahoo.com>
2022-03-21 20:37:27 +0300

    add print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * _strlen - print pointer value
+ * @str: String pointer
+ *
+ * Description: get string lenght of a pointer.
+ * Return: string length
+ */
+void _puts(char *str)
+{
+
+	puts(str);
+	puts('\n');
+}
