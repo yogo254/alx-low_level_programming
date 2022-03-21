70211fb24dade980af45bc56f1b0e2bbcd846d14
yogo254 <yogo254@yahoo.com>
2022-03-21 20:05:36 +0300

    add swap pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+/**
+ * swap_int- swap pointer values
+ * @a: first pointer
+ * @b: second poninter
+ *
+ * Description: swap a and b pointer values.
+ * Return: exist status
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	*c = *a;
+	*a = *b;
+	*b = *c;
+}
