ce5b120ea3d3373a106fa2042bcf2c6b87025338
yogo254 <yogo254@yahoo.com>
2022-03-21 19:59:34 +0300

    add update pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - change pointer value
+ * @n: current pointer
+ *
+ * Description: updates the pinter value to 98.
+ * Return: exist status
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
