c7dcf0a476d8de5a71e99d0e544ba59e9f238715
yogo254 <yogo254@yahoo.com>
2022-03-21 21:28:06 +0300

    fix puts

--- 0x05-pointers_arrays_strings/test.c
+++ 0x05-pointers_arrays_strings/test.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <string.h>
+
 int main(int argc, char const *argv[])
 {
 	char *s = "hello";
 
-	int len = strlen(s);
-
-	printf("lenght %d", len);
+	puts(s);
+	printf("\n");
 
 	return 0;
 }
