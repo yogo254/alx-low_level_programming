521660bfdb46defea01dfbf69e8ee5ce497a4509
yogo254 <yogo254@yahoo.com>
2022-03-28 09:01:37 +0300

    add get initial bytes

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include <string.h>
+/**
+ * _strchr - get initial bytes from s consisting of accept
+ * @s: source string
+ * @accept: initial bytes to accept
+
+ * Description:  get initial bytes from s consisting of accept
+ * Return: int initial bytes lenght
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int len = strlen(s);
+
+	return (len);
+}
