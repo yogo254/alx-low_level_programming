246214d45f5f1dccd4cd61e03aba363525f135a1
yogo254 <yogo254@yahoo.com>
2022-03-30 09:47:11 +0300

    added get string lenght

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _print_rev_recursion print string using reccursion
+ * _strlen_recursion get String lenght
  * @s: String
  * Description:  print string using reccursion
  * Return: void
  */
-void _print_rev_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 
 	int len = strlen(s);
 
-	for (int i = len - 1; i >= 0; i--)
-	{
-		putchar(s[i]);
-	}
+	return (len);
 }
